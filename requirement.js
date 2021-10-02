1738cccc2662ce942369e05698bf78128fd90202
Tejas-coder <tejaskaranjavkar@gmail.com>
2021-10-02 18:17:20 +0530

    Initial commit

---
+++ requirement.js
@@ -0,0 +1,63 @@
+//Requirements for the project
+
+//We are thw book management Company
+
+//BOOKS
+//ISBN, title, pubdate, lang, numpage, author[], category[]
+
+//AUTHORS
+//Id, name, books[]
+
+//PUBLICATION
+//Id, name, books[]
+
+//We have to design and code an APi over this
+
+//1. BOOKs
+//We need an API:-
+//to get all the books                          :Done
+//To get specific book                          :Done
+//to get a list of books based on category      :Done
+//To get a list of books based on languages     :Done
+
+//2. AUTHORS
+//We need an Api :-
+//To get all the authors                        :Done
+//to get spacific author                        :done
+//list of author based on books                 :Done
+
+//3. PUBLICATION
+//We need an API:-
+//to get all the publication                    :Done
+//to get aspecific publication                  :Done
+//To get a list of publications based on a book :Done
+
+
+
+//POST REQUEST
+//1. Add new book                               :Done
+//2. Add new Author                             :Done
+//3. Add new Publication                        :Done      
+
+
+
+//PUT
+//1. Update book details if author is change
+
+
+
+//Delete
+//1. Delete a book
+//2. Delete Author from book
+//3. Delete author from book and related book from author
+
+
+
+
+
+//Schema - Blueprint of how data has to be constructed
+//MongoDB is schemaless
+//mongoose has schema
+//mongoose - validation , relationship with other data.
+//model -> document model of MongoDB
+//Schema -> Model -> use them . 
